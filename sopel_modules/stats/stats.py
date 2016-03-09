057884d9cd0136585fc69dddce4522fadf735dc9
Dennis Whitney <denniswhitney@gmail.com>
2016-03-08 19:06:44 -0600

    Added print_stats and word_count function. Left print_stats open to future added stats

--- sopel_modules/stats/stats.py
+++ sopel_modules/stats/stats.py
@@ -13,14 +13,37 @@ def configure(config):
 def setup(bot):
     pass
 
+def current_count(_nick, _count_key):
+    """ Returns the word count for a given nick"""
+
+    try:
+        word_count = int(bot.db.get_nick_value(_nick, _count_key))
+    except:
+        word_count = 0
+
+    if word_count == None:
+        word_count = 0
+
+    return word_count
+
 @module.require_chanmsg()
 @module.commands("stats")
-def print_stats:
+def print_stats(bot, trigger):
     """
     Print the stats to the channel it was called from. If no nick is given
     it will display your own stats
     """
 
+    if not trigger.group(2):
+        _nick = trigger.nick
+    else:
+        _nick = str(trigger.group(2)[0])
+
+    _channel = str(trigger.sender)
+    _count_key = "stats_wcount_" + _channel
+    word_count = current_count(_nick, _count_key)
+
+    bot.say("{}'s word count in this channel is: {}").format(_nick, word_count)
 
 @module.require_chanmsg()
 @module.rule("(.*)")
@@ -33,10 +56,5 @@ def count_words(bot, trigger):
     _count_key = "stats_wcount_" + _channel
 
     bot.db.set_nick_value(_nick, "stats_timestamp_" + _channel, time.time())
-
-    try:
-        word_count = int(bot.db.get_nick_value(_nick, _count_key))
-    except:
-        word_count = 0
-
+    word_count = current_count(_nick, _count_key)
     bot.db.set_nick_value(_nick, _count_key, word_count += len(trigger))
