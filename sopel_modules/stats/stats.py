eff3bf81aa5a0a5c16e4c91dd30006121084f0ac
Dennis Whitney <denniswhitney@gmail.com>
2016-03-08 18:27:17 -0600

    added counting function

--- sopel_modules/stats/stats.py
+++ sopel_modules/stats/stats.py
@@ -3,6 +3,7 @@
 from __future__ import unicode_literals, absolute_import, division, print_function
 
 from sopel import module
+from .statsdb import *
 
 
 def configure(config):
@@ -12,7 +13,30 @@ def configure(config):
 def setup(bot):
     pass
 
+@module.require_chanmsg()
+@module.commands("stats")
+def print_stats:
+    """
+    Print the stats to the channel it was called from. If no nick is given
+    it will display your own stats
+    """
 
-@module.command('helloworld')
-def hello_world(bot, trigger):
-    bot.say('Hello, world!')
+
+@module.require_chanmsg()
+@module.rule("(.*)")
+@module.priority("low")
+def count_words(bot, trigger):
+    """Counts the total words from a specific nick in a specific channel"""
+    _nick = str(trigger.nick)
+    _channel = str(trigger.sender)
+    _message = str(trigger)
+    _count_key = "stats_wcount_" + _channel
+
+    bot.db.set_nick_value(_nick, "stats_timestamp_" + _channel, time.time())
+
+    try:
+        word_count = int(bot.db.get_nick_value(_nick, _count_key))
+    except:
+        word_count = 0
+
+    bot.db.set_nick_value(_nick, _count_key, word_count += len(trigger))
